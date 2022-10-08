0c5a6a5ddcca61fe8ef776fff85d93355e4fbb8b
Vlad <100511183+VladC82@users.noreply.github.com>
2022-10-08 09:15:34 +0100

    introducing question

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const startButton = document.getElementById(`start-btn`);
 const questionContainerElement = document.getElementById(`question-container`);
+const questionElement = document.getElementById(`question`)
+const answerButtonsElement = document.getElementById(`answer-buttons`)
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener(`click`, startGame);
 
 function startGame() {
   console.log(`Started`);
   startButton.classList.add(`hide`);
+  shuffledQuestions = questions.sort(() => Math.random() .5)
+  currentQuestionIndex = 0
   questionContainerElement.classList.remove(`hide`);
+  setNextQuestion()
 }
 
-function setNextQuestion() {}
+function setNextQuestion() {
+   showQuestion(shuffledQuestions[currentQuestionIndex]) 
+}
 
-function selectSnswer() {}
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
+function selectSnswer() {
+
+}
+
+const questions - [
+    {
+        question: `What is 5 + 5`,
+        answers:[
+            { text: `10` correct: true},
+            { text: `15` correct: false}
+        ]
+    }
+]
