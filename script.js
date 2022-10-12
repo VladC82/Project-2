ec9e61199a29c0050841b57d2fde3be2d20f6738
Vlad <100511183+VladC82@users.noreply.github.com>
2022-10-12 19:33:29 +0100

    math

--- script.js
+++ script.js
@@ -1,38 +1,87 @@
 const startButton = document.getElementById(`start-btn`);
+const nextButton = document.getElementById(`next-btn`);
 const questionContainerElement = document.getElementById(`question-container`);
-const questionElement = document.getElementById(`question`)
-const answerButtonsElement = document.getElementById(`answer-buttons`)
-let shuffledQuestions, currentQuestionIndex
+const questionElement = document.getElementById(`question`);
+const answerButtonsElement = document.getElementById(`answer-buttons`);
+
+let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener(`click`, startGame);
+nextButton.addEventListener(`click`, () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
   console.log(`Started`);
   startButton.classList.add(`hide`);
-  shuffledQuestions = questions.sort(() => Math.random() .5)
-  currentQuestionIndex = 0
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
   questionContainerElement.classList.remove(`hide`);
-  setNextQuestion()
+  setNextQuestion();
 }
 
 function setNextQuestion() {
-   showQuestion(shuffledQuestions[currentQuestionIndex]) 
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
-    questionElement.innerText = question.question
-}
-
-function selectSnswer() {
-
-}
-
-const questions - [
-    {
-        question: `What is 5 + 5`,
-        answers:[
-            { text: `10` correct: true},
-            { text: `15` correct: false}
-        ]
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement(`button`);
+    button.innerText = answer.text;
+    button.classList.add(`btn`);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
     }
-]
+    button.addEventListener(`click`, selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add(`hide`);
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer() {
+  const selectButton = e.target;
+  const correct = selectButton.datast.correct;
+  setStatusClass(document.body, correct);
+  Array.front(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove(`hide`);
+  } else {
+    startButton.innerText = `Restart`;
+    startButton.classList.remove(`hide`);
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add(`correct`);
+  } else {
+    element.classList.add(`wrong`);
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove(`correct`);
+  element.classList.remove(`wrong`);
+}
+
+const questions = [
+  {
+    question: `What is 2 + 2?`,
+    answers: [
+      { text: `4`, correct: true },
+      { text: `5`, correct: false },
+    ],
+  },
+];
